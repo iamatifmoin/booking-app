da55e1001d425811de65d4b8e5d117cd06852d21
deshdeepakkushwaha <deshdeepakkushwaha04@gmail.com>
2023-06-02 16:31:34 +0530

    add upload-by-link

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+
+const imageDownloader = require("image-downloader");
 const User = require("./models/User");
 
 require("dotenv").config();
@@ -13,6 +15,7 @@ const jwtSecret = "fasefraw4r5r3wq45wdfgw34twdfg";
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
@@ -93,6 +96,17 @@ app.get("/profile", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json(true);
+});
+
+app.post("/upload-by-link", async (req, res) => {
+  const { link } = req.body;
+  const newName = "photo" + Date.now() + ".jpg";
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + "/uploads/" + newName,
+  });
+
+  res.json(newName);
 });
 
 app.listen(4000, () => {
